12618dc7fe5162a408eb5cfea5deebd656b4671a
HoangPhuong01 <won.vn2000@yahoo.com>
2019-10-27 16:02:42 +0700

    Update

--- Week 3/Rock_Paper_Sicssors/App.js
+++ Week 3/Rock_Paper_Sicssors/App.js
@@ -7,40 +7,127 @@
  */
 
 import React, { Component } from 'react';
-import { Text, View,StyleSheet,TouchableOpacity,Alert} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Alert, Image } from 'react-native';
 import Button from './src/Component/Button'
+import { green } from 'ansi-colors';
 
 export default class HelloWorldApp extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      userChoose: '',
-      gamePrompt: 'Choose your weapon!'
+      userChoose: 'Rock',
+      gamePrompt: 'Choose your weapon!',
+      computerChoose: 1
+
+
     }
   }
 
 
 
-  onPress = (_userChoose) => {
-    this.setState({ userChoose : _userChoose });
-    console.log("userChoise",userChoose)
+  onPress = (userChoose) => {
+    this.setState({ userChoose }, () => {
+      this.setState({ computerChoose: Math.floor(Math.random() * 3) + 1 }, () => {
+        this.gameLogic();
+      })
+    });
+
+
+
+
   }
-  
+
+  gameLogic = () => {
+    let computerText = CHOICES.find(item => item.id === this.state.computerChoose)
+
+    if (computerText.name === this.state.userChoose) {
+      this.setState({ gamePrompt: "Tie!" })
+      return;
+    }
+    else {
+      if (this.state.userChoose == "Rock") {
+        this.setState({ gamePrompt: this.state.computerChoose == 3 ? "Victory!" : "Defeat!" }, () => {
+
+        });
+        return;
+      }
+      if (this.state.userChoose == "Paper") {
+        this.setState({ gamePrompt: this.state.computerChoose == 1 ? "Victory!" : "Defeat!" })
+        return;
+
+      }
+
+      if (this.state.userChoose == "Scissors") {
+        this.setState({ gamePrompt: this.state.computerChoose == 2 ? "Victory!" : "Defeat!" })
+        return;
+      }
+    }
+  }
+
+  changePic = (userText) => {
+    let itemNeeded = CHOICES.find(item => item.name === userText)
+    return itemNeeded;
+  }
+
+  textResultColor = () => {
+    if (this.state.gamePrompt === 'Victory!') return 'green';
+    if (this.state.gamePrompt === 'Defeat!') return 'red';
+    return null;
+  }
+
+
   render() {
+
+    const userText = this.state.userChoose
+    const computerText = CHOICES.find(item => item.id === this.state.computerChoose)
     return (
       <View style={styles.container}>
-         <Text>{this.state.gamePrompt}</Text>
-         {
-           CHOICES.map((item,index)=>{
-              return  <Button 
-              name = {item.name} 
-              key= {index}
-              onPress = {this.onPress}
+        <View style={styles.header}>
+          <Text style={{ color:this.textResultColor(),fontSize:35 }}>{this.state.gamePrompt}</Text>
+        </View>
+
+        <View style={styles.body}>
+          <View style={styles.choiceContainer}>
+            <Text style={styles.choiceDescription}>
+              Player
+            </Text>
+            <Image
+              source={{ uri: this.changePic(userText).uri }}
+              resizeMode="contain"
+              style={styles.choiceImage}
+            />
+            <Text style={styles.choiceCardTitle}>
+              {userText}
+            </Text>
+          </View>
+          <Text style={{ color: '#250902' }}>vs</Text>
+          <View style={styles.choiceContainer}>
+            <Text style={styles.choiceDescription}>
+              Computer
+            </Text>
+            <Image
+              source={{ uri: computerText.uri }}
+              resizeMode="contain"
+              style={styles.choiceImage}
+            />
+            <Text style={styles.choiceCardTitle}>
+              {computerText.name}
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.footer}>
+          {
+            CHOICES.map((item, index) => {
+              return <Button
+                name={item.name}
+                key={index}
+                onPress={this.onPress}
               />
-           })
-         }
-        
-        
+            })
+          }
+        </View>
+
       </View>
     );
   }
@@ -51,15 +138,18 @@ export default class HelloWorldApp extends Component {
 
 const CHOICES = [
   {
-    name: 'rock',
+    id: 1,
+    name: 'Rock',
     uri: 'http://pngimg.com/uploads/stone/stone_PNG13622.png'
   },
   {
-    name: 'paper',
+    id: 2,
+    name: 'Paper',
     uri: 'https://www.stickpng.com/assets/images/5887c26cbc2fc2ef3a186046.png'
   },
   {
-    name: 'scissors',
+    id: 3,
+    name: 'Scissors',
     uri:
       'http://pluspng.com/img-png/png-hairdressing-scissors-beauty-salon-scissors-clipart-4704.png'
   }
@@ -73,6 +163,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#e9ebee'
   },
+
+  header: {
+    flex: 0.1,
+
+  },
+
+
+  body: {
+    flex: 0.58,
+    backgroundColor: 'white',
+    borderWidth: 2,
+    paddingTop: 10,
+    shadowRadius: 5,
+    paddingBottom: 10,
+    borderColor: 'grey',
+    shadowOpacity: 0.90,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'space-around',
+    shadowColor: 'rgba(0,0,0,0.2)',
+    shadowOffset: { height: 5, width: 5 },
+  },
+
+  footer: {
+    flex: 0.32,
+
+  },
+
   buttonContainer: {
     alignItems: 'center',
     justifyContent: 'center',
@@ -92,19 +211,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   choicesContainer: {
-    margin: 10,
-    borderWidth: 2,
-    paddingTop: 100,
-    shadowRadius: 5,
-    paddingBottom: 100,
-    borderColor: 'grey',
-    shadowOpacity: 0.90,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    justifyContent: 'space-around',
-    shadowColor: 'rgba(0,0,0,0.2)',
-    shadowOffset: { height: 5, width: 5 },
+    // flex: 1,
+    // margin: 10,
+    // borderWidth: 2,
+    // paddingTop: 100,
+    // shadowRadius: 5,
+    // paddingBottom: 100,
+    // borderColor: 'grey',
+    // shadowOpacity: 0.90,
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    // backgroundColor: 'white',
+    // justifyContent: 'space-around',
+    // shadowColor: 'rgba(0,0,0,0.2)',
+    // shadowOffset: { height: 5, width: 5 },
   },
   choiceContainer: {
     flex: 1,
---
+++ Week 5/Lab/Weather/App.js
@@ -0,0 +1,158 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+
+    this.getLocationAsync();
+  }
+
+  
+
+  getLocationAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+   
+    if (status !== "granted") {
+      return;
+    }
+
+    const location = await Location.getCurrentPositionAsync();
+    this.getWeather(10.817141, 106.707954);
+  };
+
+ 
+
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "black"
+  },
+  container: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  weatherContainer: {
+    padding: 20,
+    width: "90%",
+    borderWidth: 1,
+    maxWidth: "90%",
+    minHeight: "20%",
+    marginTop: "70%",
+    borderRadius: 25,
+    marginBottom: "2%",
+    borderColor: "white",
+    backgroundColor: "rgba(0,0,0,0.4)"
+  },
+  text: {
+    fontSize: 20,
+    color: "white",
+    marginLeft: 10,
+    fontWeight: "bold"
+  },
+  row: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  cityContainer: {
+    flex: 1,
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  cityName: {
+    fontSize: 12,
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  cityButton: {
+    margin: 3,
+    height: 40,
+    padding: 3,
+    width: "25%",
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.5)"
+  },
+  tempRow: {
+    alignSelf: "center",
+    flexDirection: "row"
+  },
+  locationText: {
+    fontSize: 25,
+    color: "white",
+    marginLeft: 10,
+    fontWeight: "bold",
+    textDecorationLine: "underline"
+  },
+  loading: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  currentLocation: {
+    margin: 3,
+    height: 40,
+    padding: 3,
+    width: "72.5%",
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(20,33,61,0.6)"
+  }
+});
+
+
